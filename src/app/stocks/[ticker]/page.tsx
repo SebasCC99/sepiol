21f44b41c9fdc8b9b9fa6aa3fbd3cf590a5435f5
Sebastian Carazo <sebastian@lebas.dev>
2024-10-21 09:30:52 -0500

    add dynamic fetching of tickers

--- src/app/stocks/[ticker]/page.tsx
+++ src/app/stocks/[ticker]/page.tsx
@@ -1,7 +1,9 @@
-export default async function Tickers() {
-  fetch("http://localhost:8080/stocks/appl");
+export default async function Page({params}: {params : {ticker: string}}) {
+  fetch(`http://localhost:8080/stocks/${params.ticker}`);
+
   return (
     <main>      
+      <h1>Ticker: {params.ticker.toUpperCase()}</h1>
     </main>
   );
 }
