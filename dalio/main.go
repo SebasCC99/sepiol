24c79aabd03f165959c1f421a0d974e33a5e4973
Sebastian Carazo <sebastian@lebas.dev>
2024-10-21 08:37:00 -0500

    fetch polygon api when navigating to dynamic stocks route

--- dalio/main.go
+++ dalio/main.go
@@ -1,16 +1,35 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+
+	polygon "github.com/polygon-io/client-go/rest"
+	"github.com/polygon-io/client-go/rest/models"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello from Go server!")
+
+	c := polygon.New("API_KEY")
+
+	params := models.GetTickerDetailsParams{
+		Ticker: "AAPL",
+	}
+
+	res, err := c.GetTickerDetails(context.Background(), &params)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Print(res)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/stocks/appl", handler)
 
 	fmt.Println("Server is starting on port 8080...")
 	err := http.ListenAndServe(":8080", nil)
---
+++ src/app/stocks/[ticker]/page.tsx
@@ -0,0 +1,7 @@
+export default async function Tickers() {
+  fetch("http://localhost:8080/stocks/appl");
+  return (
+    <main>      
+    </main>
+  );
+}
